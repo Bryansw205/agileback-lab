e0ad37582c8a523d6683347035894d63a60cbe1d
Bryan Mariños <bryanmg.020@gmail.com>
2025-11-28 01:51:20 -0500

    cambios

--- models/db.js
+++ models/db.js
@@ -1,17 +1,19 @@
+require('dotenv').config();
 const mysql = require('mysql2');
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root', // Tu usuario de MySQL
-  password: '3166', // Tu contraseña de MySQL
-  database: 'tienda',
+  host: 'aws-0-us-west-2.pooler.supabase.com',
+  user: 'postgres.vtigceezhzxalumwlbsz',
+  password: 'bd123',
+  database: 'postgres',
+  port: 5432,
 });
 
 db.connect((err) => {
   if (err) {
     console.error('Error al conectar a la base de datos:', err);
   } else {
-    console.log('Conectado a la base de datos');
+    console.log('Conectado a la base de datos Supabase');
   }
 });
 
